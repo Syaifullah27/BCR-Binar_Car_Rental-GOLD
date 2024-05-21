c66cd9c6be367ab0fadd12a04e0380102f484a76
Esyeha <syh48149@gmail.com>
2024-05-21 14:22:04 +0700

    make main section

--- src/Components/MainSection/index.jsx
+++ src/Components/MainSection/index.jsx
@@ -3,8 +3,17 @@ import "./mainSection.css"
 const MainSection = ({ isOpen }) => {
 
     return (
-        <div className={`main-section-container ${isOpen ? 'blur' : ''}`}>
-            <h1 className="title">Main Section</h1>
+        <div className={`main-section-wrapper ${isOpen ? 'blur' : ''}`}>
+            <div className="main-section-container">
+                <div className="left-main-section">
+                    <h1 className="title">Sewa & Rental Mobil Terbaik Di Kawasan (Lokasimu)</h1>
+                    <p className="desc">Selamat datang di Binar Car Rental. Kami menyediakan mobil kualitas terbaik dengan harga terjangkau. Selalu siap melayani kebutuhanmu untuk sewa mobil selama 24 jam.</p>
+                    <button className="btn">Mulai Sewa Mobil</button>
+                </div>
+                <div className="right-main-section">
+                    <img src="./images/img_car.png" alt="main-img" />
+                </div>
+            </div>
         </div>
     )
 }
