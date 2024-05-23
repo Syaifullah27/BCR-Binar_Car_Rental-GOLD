7797bfefa48820878e5093ab869010bf6d52e837
Esyeha <syh48149@gmail.com>
2024-05-23 13:07:20 +0700

    fix whyUs section

--- src/Components/testimoniSection/index.jsx
+++ src/Components/testimoniSection/index.jsx
@@ -1,57 +1,103 @@
-import { useState } from "react";
+// import { useState } from "react";
 import "./testimoni.css"
 import { dataTestimoni } from "../../Utils/DumyData";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
+
+
+function SampleNextArrow(props) {
+    const { onClick } = props;
+    return (
+        <div
+            className="next-arrow"
+            onClick={onClick}
+        />
+    );
+}
+
+function SamplePrevArrow(props) {
+    const { onClick } = props;
+    return (
+        <div
+            className="prev-arrow"
+            onClick={onClick}
+        />
+    );
+}
+
+
+
+
+
 // eslint-disable-next-line react/prop-types
 const TestimoniSection = ({ isOpen }) => {
+    const settings = {
+        infinite: false,
+        speed: 1000,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        initialSlide: 0,
+        prevArrow: <SamplePrevArrow />,
+        nextArrow: <SampleNextArrow />,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    initialSlide: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    };
 
-    
 
-        const [currentIndex, setCurrentIndex] = useState(0);
-
-        const handlePrev = () => {
-            setCurrentIndex(prevIndex =>
-                prevIndex === 0 ? dataTestimoni.length - 1 : prevIndex - 1
-            );
-        };
-
-        const handleNext = () => {
-            setCurrentIndex(prevIndex =>
-                prevIndex === dataTestimoni.length - 1 ? 0 : prevIndex + 1
-            );
-        };
-
-
-        return (
-            <div className={`${isOpen ? 'blur' : ''}`}>
-                <div className="testimoni-wrapper">
-                    <div className="testimoni-container">
-                        <h1>Testimonial</h1>
-                        <p>Berbagai review positif dari para pelanggan kami</p>
-                        <div className="testimoni-card-container">
-                            {
-                                dataTestimoni.map((testimoni, index) => (
-                                    <div className={`testimoni-card ${index === currentIndex ? 'active' : ''}`} key={index}>
-                                        <img src={testimoni.img} alt="" />
-                                        <div className="text">
-                                            <img src={testimoni.rate} alt="" />
-                                            <p>{testimoni.desc}</p>
-                                            <h3>{testimoni.title}</h3>
-                                        </div>
-                                    </div>
-                                ))
-                            }
+    return (
+        <div className={`${isOpen ? 'blur' : ''}`}>
+            <div className="testimoni-container">
+                <div className="slider-container">
+                    <Slider {...settings}>
+                        <div className="testimoni-card">
+                            <h3>1</h3>
+                            <img src="./images/images.jpeg" alt="" />
                         </div>
-                        <div className="button-container">
-                            <button className="prev-btn" onClick={handlePrev}>
-                                &#10094;
-                            </button>
-                            <button className="next-btn" onClick={handleNext}>
-                                &#10095;
-                            </button>
+                        <div className="testimoni-card">
+                            <h3>2</h3>
+                            <img src="./images/images.jpeg" alt="" />
                         </div>
-                    </div>
+                        <div className="testimoni-card">
+                            <h3>3</h3>
+                            <img src="./images/images.jpeg" alt="" />
+                        </div>
+                        <div className="testimoni-card">
+                            <h3>4</h3>
+                            <img src="./images/images.jpeg" alt="" />
+                        </div>
+                        <div className="testimoni-card">
+                            <h3>5</h3>
+                            <img src="./images/images.jpeg" alt="" />
+                        </div>
+                    </Slider>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
+}
 export default TestimoniSection
