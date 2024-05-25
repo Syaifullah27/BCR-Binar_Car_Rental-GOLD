9461f26d7f13fdfdc53a5050daf3f4ed5d64a316
Esyeha <syh48149@gmail.com>
2024-05-25 10:42:40 +0700

    make folder pages for routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,15 @@
-import { useState } from 'react'
-import './App.css'
-import MainSection from './Sections/MainSection'
-import Navbar from '../src/Components/Navbar'
-import SecondSection from './Sections/SecondSection'
-import WhyUsSection from './Sections/whyUsSection'
-import TestimoniSection from './Sections/testimoniSection'
-import Banner from './Sections/Banner'
-import FaqSections from './Sections/Faq'
-import Footer from './Components/Footer'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import HomePage from "./Pages/Home/Home"
 
 const App = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false)
-  
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen)
-  }
-
-  // tutup kembali jika menu jika klik diluar menu
-  // const closeMenu = () => {
-  //   if (isMenuOpen) {
-  //     setIsMenuOpen(false)
-      
-  //   }
-  // }
 
   return (
-    // <div className="App" onClick={closeMenu}>
     <div>
-      <Navbar isOpen={isMenuOpen} toggleMenu={toggleMenu} setIsMenuOpen={setIsMenuOpen}/>
-      <MainSection isOpen={isMenuOpen} btn={true}/>
-      <SecondSection isOpen={isMenuOpen}/>
-      <WhyUsSection isOpen={isMenuOpen}/>
-      <TestimoniSection isOpen={isMenuOpen}/>
-      <Banner isOpen={isMenuOpen}/>
-      <FaqSections isOpen={isMenuOpen}/>
-      <Footer isOpen={isMenuOpen}/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import "../../../src/App.css"
+import MainSection from '../../Sections/MainSection'
+import Navbar from '../../Components/Navbar'
+import SecondSection from '../../Sections/SecondSection'
+import WhyUsSection from '../../Sections/whyUsSection'
+import TestimoniSection from '../../Sections/testimoniSection'
+import Banner from '../../Sections/Banner'
+import FaqSections from '../../Sections/Faq'
+import Footer from '../../Components/Footer'
+
+const HomePage = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen)
+    }
+
+    // tutup kembali jika menu jika klik diluar menu
+    // const closeMenu = () => {
+    //   if (isMenuOpen) {
+    //     setIsMenuOpen(false)
+
+    //   }
+    // }
+
+    return (
+        // <div className="App" onClick={closeMenu}>
+        <div>
+            <Navbar isOpen={isMenuOpen} toggleMenu={toggleMenu} setIsMenuOpen={setIsMenuOpen} />
+            <MainSection isOpen={isMenuOpen} btn={true} />
+            <SecondSection isOpen={isMenuOpen} />
+            <WhyUsSection isOpen={isMenuOpen} />
+            <TestimoniSection isOpen={isMenuOpen} />
+            <Banner isOpen={isMenuOpen} />
+            <FaqSections isOpen={isMenuOpen} />
+            <Footer isOpen={isMenuOpen} />
+        </div>
+    )
+}
+
+export default HomePage
