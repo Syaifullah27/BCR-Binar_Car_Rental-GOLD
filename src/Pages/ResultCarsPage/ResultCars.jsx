9f0d07913772ec3bf71d1011b952c489146c621b
Esyeha <syh48149@gmail.com>
2024-05-29 13:19:41 +0700

    make car card in result pages

--- src/Pages/ResultCarsPage/ResultCars.jsx
+++ src/Pages/ResultCarsPage/ResultCars.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react"
 import Navbar from "../../Components/Navbar"
 import ResultCarsTable from "../../Sections/ResultCarsTable"
+import Footer from "../../Components/Footer"
+import CardCars from "../../Sections/CardCars"
 
 const ResultCars = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false)
@@ -14,6 +16,10 @@ const ResultCars = () => {
         <div>
             <Navbar isOpen={isMenuOpen} toggleMenu={toggleMenu} setIsMenuOpen={setIsMenuOpen} logoBtn={false}/>
             <ResultCarsTable isOpen={isMenuOpen}/>
+            <CardCars isOpen={isMenuOpen}/>
+            <div style={{marginTop: "120px"}}>
+            <Footer isOpen={isMenuOpen}/>
+            </div>
         </div>
     )
 }
---
+++ src/Sections/CardCars/index.jsx
@@ -0,0 +1,33 @@
+import "./cardcars.css"
+
+const CardCars = () => {
+    return (
+        <div className="card-cars-wrapper">
+            <div className="card-cars-container">
+                <div className="card-cars">
+                    <img src="./images/cars.png" alt="cars" />
+                    <p className="title-cars">Inova</p>
+                    <h4>Rp 500.000 / hari</h4>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus id nulla, provident ea voluptas laudantium unde.</p>
+                    <button className="card-cars-btn">Pilih Mobil</button>
+                </div>
+                <div className="card-cars">
+                    <img src="./images/cars.png" alt="cars" />
+                    <p className="title-cars">Inova</p>
+                    <h4>Rp 500.000 / hari</h4>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus id nulla, provident ea voluptas laudantium unde.</p>
+                    <button className="card-cars-btn">Pilih Mobil</button>
+                </div>
+                <div className="card-cars">
+                    <img src="./images/cars.png" alt="cars" />
+                    <p className="title-cars">Inova</p>
+                    <h4>Rp 500.000 / hari</h4>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus id nulla, provident ea voluptas laudantium unde.</p>
+                    <button className="card-cars-btn">Pilih Mobil</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CardCars
