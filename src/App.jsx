51246d02c9c410ef5615c4f981c98712b8683f45
Esyeha <syh48149@gmail.com>
2024-06-01 16:52:44 +0700

    fix search bar & detail page

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,40 @@ import HomePage from "./Pages/Home/Home"
 import RentalsPage from "./Pages/RentalCarsPage/rentalsCars"
 import ResultCars from "./Pages/ResultCarsPage/ResultCars"
 import DetailCars from "./Pages/DetailCarsPage/DetailCars"
+import { useState } from "react"
 
 const App = () => {
   
+  const [nameCar, setNameCar] = useState('')
+  const [capacityCar, setCapacityCar] = useState('')
+  const [priceCar, setPriceCar] = useState('')
+  const [statusCar, setStatusCar] = useState('')
+
   return (
     <div>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage inMenu={true} />} />
-          <Route path="/search-cars" element={<RentalsPage inMenu={true} />} />
-          <Route path="/result-cars/" element={<ResultCars />} />
+          <Route path="/search-cars" element={<RentalsPage inMenu={true} 
+          nameCar={nameCar}
+          capacityCar={capacityCar}
+          priceCar={priceCar}
+          statusCar={statusCar}
+          setNameCar={setNameCar}
+          setCapacityCar={setCapacityCar}
+          setPriceCar={setPriceCar}
+          setStatusCar={setStatusCar}
+          />} />
+          <Route path="/result-cars/" element={<ResultCars />} 
+          nameCar={nameCar}
+          capacityCar={capacityCar}
+          priceCar={priceCar}
+          statusCar={statusCar}
+          setNameCar={setNameCar}
+          setCapacityCar={setCapacityCar}
+          setPriceCar={setPriceCar}
+          setStatusCar={setStatusCar}
+          />
           <Route path="/detail-car/:id" element={<DetailCars />} />
         </Routes>
       </BrowserRouter>
--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -1,8 +1,17 @@
 import { Link } from "react-router-dom"
 import "./button.css"
 const Button = () => {
+
+    const scroolToTop = () => {
+        window.scrollTo({
+            top: 0,
+        });
+    };
+
     return (
-        <Link className="btn" to={"/search-cars"}>Mulai Sewa Mobil</Link>
+        <Link to={"/search-cars"} style={{ textDecoration: "none" }}>
+            <button onClick={() => scroolToTop()} className="btn">Mulai Sewa Mobil</button>
+        </Link>
     )
 }
 
